a4b231bd7a2b02a72a4acf0efbbfaa34cfd603b9
2202Hannah <hannahbarber22@hotmail.co.uk>
2022-11-19 20:18:57 +0000

    change listen.js

--- listen.js
+++ listen.js
@@ -1,8 +1,6 @@
 const app = require("./app.js");
 require("dotenv").config();
 
-//const { PORT = 9090 } = process.env;
+const { PORT = 9090 } = process.env;
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Listening on ${PORT}...`)
-);
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
