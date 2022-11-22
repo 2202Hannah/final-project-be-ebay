d77edd7ecc100839fafee5eb50e066048c7caa52
2202Hannah <hannahbarber22@hotmail.co.uk>
2022-11-22 11:22:13 +0000

    allow cross origin resource sharing

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const { getEbayItems } = require("./controllers");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.json());
 app.get("/", (req, res) => {
